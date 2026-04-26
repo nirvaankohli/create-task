2c720d9787c952f2e741ca56be1e6554e33255b3
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2026-04-26 12:52:57 -0400

    Made /create-game logic along w function for random game ID

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const WebSocket = require("ws");
 const { Chess } = require("chess.js");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
 const server = http.createServer(app);
@@ -11,8 +14,30 @@ const wss = new WebSocket.Server({ server });
 
 games = {};
 
+function randomGameID() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
 app.get("/", (req, res) => {
   res.send("EzChess Server is running.");
+});
+
+app.post("/create-game", (req, res) => {
+  color = req.body.color;
+  player_cookie_hash = req.body.player_cookie_hash;
+
+  gameID = randomGameID();
+  games[gameID] = {
+    id: gameID,
+    chess: new Chess(),
+    player1: {
+      cookie_hash: player_cookie_hash,
+      color: color,
+    },
+    player2: null,
+    turn: "w",
+    state: "waiting_for_player",
+  };
 });
 
 app.listen(PORT, () => {
