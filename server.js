d5ba5e009a57ab2ba79f382e7aa749597f1340d1
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2026-04-26 12:34:46 -0400

    Websocket + Chess.js to server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const { Chess } = require("chess.js");
+
 const app = express();
 const PORT = 3000;
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+games = {};
 
 app.get("/", (req, res) => {
   res.send("EzChess Server is running.");
